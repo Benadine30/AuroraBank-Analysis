6086452041dea0f7111f3fa34c64993f9e3518a7
Anozie (Ibeh) Chinonso Benadine <105176448+Benadine30@users.noreply.github.com>
2025-10-21 11:10:07 +0200

    Delete SQLQuery_AuroraBank.sql

--- SQLQuery_AuroraBank.sql
+++
@@ -1,8 +0,0 @@
-
-SELECT * FROM cards_data
-SELECT * FROM transactions_data
-SELECT * FROM users_data
-SELECT * FROM mcc_codes
-
-ALTER TABLE transactions_data
-ADD CONSTRAINT PK_transactions_data PRIMARY KEY (id);
